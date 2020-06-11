d4ffcc3f4f94ee18ea026a32c18068e5a6c0dc77
vagrant <vagrant@localhost.localdomain>
2020-06-11 09:36:44 +0000

    Finish-post

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to "/books"
   end
 
   def show
@@ -7,4 +14,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/show'
-  get 'books/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/" => "homes#top"
+  get "books" => "books#new"
+  post "books" => "books#create"
+
 end
